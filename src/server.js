b5a46e6f404718d9393b97810a93d0d41b45cc32
binglinzhang <1979742455@qq.com>
2017-06-14 21:18:40 +0800

    新增筛选功能/多路数据请求/局部滚动等

---
+++ src/server.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const dist = require('./dictionary.json')
+const request = require('request')
+
+// app.get('/test', (req,res) => {
+//     console.log(req.url)
+//     res.send('good')
+// })
+
+let
+    promiseArr = [],
+    responseArr = []
+app.get('/queryData', (req, res) => {
+    let keyWordsArr = req.query.keyWords
+    keyWordsArr.forEach(keyword => {
+        let url = `http://api.tmkoo.com/search.php?keyword=${keyword}&apiKey=4399320012393234&apiPassword=331nd3342d&pageSize=30&pageNo=1`
+        promiseArr.push(new Promise((resolve, reject) => {
+            request(encodeURI(url), (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    responseArr.push(body)
+                    resolve()
+                }
+            })
+        }))
+    })
+    Promise.all(promiseArr).then(() => {
+        console.log(responseArr)
+        res.send(responseArr)
+    }).catch((err) => {
+        console.log(err)
+        res.send('error')
+    })
+})
+
+
+let
+    result = [],
+    keysArr = [],
+    flag = 0
+dist.forEach(obj => {
+    keysArr.push(Object.keys(obj)[0])
+})
+
+app.get('/search', (req, res) => {
+    result = []
+    req.query.searchContent.forEach(val => {
+        if (val) { //过滤空格
+            handleData(val)
+        }
+    })
+    flag === 1 ? res.send(result) : res.send('0')
+})
+
+function handleData(inputVal) {
+    for (let i = 0; i < keysArr.length; i++) {
+        let key = keysArr[i]
+        if (inputVal === key) {
+            for (let j = 0; j < dist.length; j++) {
+                if (dist[j][key]) {
+                    let tempObj = {}
+                    tempObj.key = key
+                    tempObj.value = dist[j][key]
+                    result.push(tempObj)
+                    flag = 1
+                    break
+                }
+            }
+            if (flag === 1) {
+                break
+            }
+        } else {
+            flag = 0
+        }
+    }
+}
+
+app.use(express.static(require('path').join(__dirname, '.')))
+app.listen(8888, () => {
+    console.log('8888 started...')
+})
